e1b075acb4fc2e342b224f229eb9241f1a9be87d
enzoben <enzobenbalit@gmail.com>
2025-01-01 22:09:48 +0100

    ajout du wave Unet

--- extract_data.py
+++ extract_data.py
@@ -222,14 +222,14 @@ def plot_amplitude(path_to_signal):
 
     plt.show()
 
-def plotlosses_history(df):
+def plotlosses_history(df, model_name):
 
     plt.figure(figsize=(8, 5))
     plt.plot(df.index +1, df['train_loss'], label='Train Loss', color='blue', marker='o')
     plt.plot(df.index+1, df['valid_loss'], label='Valid Loss', color='orange', marker='x')
 
     # Ajout de titres et de légendes
-    plt.title('Train and Validation Loss for UNet with L1 loss')
+    plt.title('Train and Validation Loss for {}'.format(model_name))
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
@@ -238,7 +238,7 @@ def plotlosses_history(df):
     # Affichage
     plt.show()
 
-def SISDR_by_SNR(model, test_data , affichage = True, model_name = "nom du model", device = 'cpu'):
+def SISDR_by_SNR(model, test_data , affichage = True, model_name = "nom du model", device = 'cpu', spectrogram = True):
 
     device = torch.device(device)
     model.to(device)
@@ -253,12 +253,16 @@ def SISDR_by_SNR(model, test_data , affichage = True, model_name = "nom du model
         voice_audio, _ = torchaudio.load(path_to_data+'/voice.wav')
         mix_audio, _ = torchaudio.load(path_to_data+'/mix_snr_{:.0f}.wav'.format(snr))
 
-        Sxx_mix = torch.stft(mix_audio, n_fft=800, window=torch.hann_window(800), return_complex=True).unsqueeze(0).to(device)
-        Sxx_mix_ampl = torch.abs(Sxx_mix)
-        pred = model(Sxx_mix_ampl)
+        if spectrogram:
+            Sxx_mix = torch.stft(mix_audio, n_fft=800, window=torch.hann_window(800), return_complex=True).unsqueeze(0).to(device)
+            Sxx_mix_ampl = torch.abs(Sxx_mix)
+            pred = model(Sxx_mix_ampl)
 
-        Sxx_voice_reconstruct = pred*Sxx_mix/Sxx_mix_ampl
-        audio_reconstruct = torch.istft(Sxx_voice_reconstruct[0,:,:].to('cpu'), n_fft = 800, window=torch.hann_window(800))
+            Sxx_voice_reconstruct = pred*Sxx_mix/Sxx_mix_ampl
+            audio_reconstruct = torch.istft(Sxx_voice_reconstruct[0,:,:].to('cpu'), n_fft = 800, window=torch.hann_window(800))
+        
+        else :
+            audio_reconstruct = model(mix_audio.unsqueeze(0).to(device))
 
         si_sdr_dict[str(int(snr))].append(float(si_sdr(voice_audio,audio_reconstruct).detach().numpy()))
 
@@ -285,3 +289,93 @@ def plot_SISDR_by_SNR(si_sdr_dict, model_name):
 
     # Affichage
     plt.show() 
+
+def interactive_reconstruction(df, model):
+    # Trier les SNR par ordre croissant
+    sorted_snr = sorted(df["SNR"].unique())
+
+    # Créer un menu déroulant pour les SNR uniques, trié
+    snr_dropdown = widgets.Dropdown(
+        options=sorted_snr,
+        value=sorted_snr[0],  # Sélectionner le premier élément par défaut
+        description="SNR:",
+    )
+
+    # Créer un menu déroulant pour les noms (sera mis à jour dynamiquement)
+    name_dropdown = widgets.Dropdown(
+        options=[],  # Initialement vide
+        value=None,
+        description="Nom:",
+    )
+
+    # Mettre à jour les options du menu déroulant des noms lorsque le SNR change
+    def update_names(*args):
+        selected_snr = snr_dropdown.value
+        filtered_names = sorted(df[df["SNR"] == selected_snr]["Nom"].tolist())  # Trier les noms
+        name_dropdown.options = filtered_names
+        if filtered_names:  # Pré-sélectionner le premier nom si disponible
+            name_dropdown.value = filtered_names[0]
+
+    # Afficher le spectrogramme immédiatement lorsque le SNR ou le Nom change
+    def on_value_change(change):
+        clear_output(wait=True)  # Effacer la sortie précédente
+        display(snr_dropdown, name_dropdown)  # Réafficher les widgets
+        selected_snr = snr_dropdown.value
+        selected_name = name_dropdown.value
+
+        # Récupérer le signal correspondant
+        path_to_signal = df[(df["SNR"] == selected_snr) & (df["Nom"] == selected_name)]["Path"].values[0]
+        plot_reconstruction(model, path_to_signal)
+
+    # Lier la mise à jour des noms au changement du SNR
+    snr_dropdown.observe(update_names, names="value")
+    name_dropdown.observe(on_value_change, names="value")  # Observer les changements dans le nom
+
+    # Initialiser les options de noms
+    update_names()
+
+    # Afficher les widgets
+    display(snr_dropdown, name_dropdown)
+
+def plot_reconstruction(model, path_to_signal):
+
+    model.to("cpu")
+
+    signal = load_signal(path_to_signal)
+
+    Smix = torch.stft(torch.tensor(signal.mix["audio"]), n_fft = 800, window = torch.hann_window(800), return_complex = True).unsqueeze(0)
+    Svoice = torch.stft(torch.tensor(signal.voice["audio"]), n_fft = 800, window = torch.hann_window(800), return_complex = True).unsqueeze(0)
+
+    Smix_ampl = torch.abs(Smix)
+    Svoice_ampl = torch.abs(Svoice)
+    mask = model(Smix_ampl.unsqueeze(0))
+    svoice_reconst = mask*Smix/Smix_ampl
+
+    fig, axes = plt.subplots(2,3, figsize = (25,12))
+
+    pcm0 = axes[0,0].pcolormesh(20*np.log10(np.abs(Smix[0,:,:].detach().numpy())), cmap='viridis')
+    fig.colorbar(pcm0, ax = axes[0,0])
+    axes[0,0].set_title("Spectrogramme du mix")
+
+    pcm1 = axes[0,1].pcolormesh(20*np.log10(np.abs(Svoice[0,:,:].detach().numpy())), cmap='viridis')
+    fig.colorbar(pcm1, ax = axes[0,1])
+    axes[0,1].set_title("Spectrogramme du ground truth")
+
+    pcm2 = axes[0,2].pcolormesh(20*np.log10(np.abs(svoice_reconst[0,0,:,:].detach().numpy())), cmap='viridis')
+    fig.colorbar(pcm2, ax = axes[0,2])
+    axes[0,2].set_title("Spectrogramme de la voix reconstruite")
+
+    audio = torch.istft(svoice_reconst[0,0,:,:].to('cpu'), n_fft = 800, window=torch.hann_window(800))
+
+    time = np.array(list(range(80000)))/8000
+    axes[1,0].plot(time, signal.mix["audio"])
+    axes[1,0].set_title("Mixte Signal")
+
+    axes[1,1].plot(time, signal.voice["audio"])
+    axes[1,1].set_title("Voice Signal")
+
+    axes[1,2].plot(time, audio.detach().numpy())
+    axes[1,2].set_title("Reconstructed Voice Signal")
+
+    plt.tight_layout()
+    plt.show()
